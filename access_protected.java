486ff0ab0cd62a020f91150ecd1ec11eaebfbb89
Kalyani <skalyaniraja@gmail.com>
2017-10-30 18:23:22 -0700

    Add files via upload

---
+++ access_protected.java
@@ -0,0 +1,12 @@
+package test_package;
+import java_modifiers.modi_protected;
+public class access_protected extends modi_protected{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		modi_protected obj = new modi_protected();
+		
+		System.out.println("Cuberoot is:");
+	}
+
+}
---
+++ access_public.java
@@ -0,0 +1,12 @@
+package test_package;
+import java_modifiers.modi_public;
+public class access_public {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		modi_public obj = new modi_public();
+		obj.sum(120, 180);
+	}
+
+}
